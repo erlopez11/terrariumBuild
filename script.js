8c999bf1f2e417296e7a1fdeee294e9db30f5c76
Your Nameerlopez11 <lopezelainy@gmail.com>
2023-10-03 10:47:43 -0500

    add all plants as DOM elements and add dragElement fn to them so they can be dragged anywhere on page

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const plant1 = document.getElementById('plant1');
+const plant2 = document.getElementById('plant2');
+const plant3 = document.getElementById('plant3');
+const plant4 = document.getElementById('plant4');
+const plant5 = document.getElementById('plant5');
+const plant6 = document.getElementById('plant6');
+const plant7 = document.getElementById('plant7');
+const plant8 = document.getElementById('plant8');
+const plant9 = document.getElementById('plant9');
+const plant10 = document.getElementById('plant10');
+const plant11 = document.getElementById('plant11');
+const plant12 = document.getElementById('plant12');
+const plant13 = document.getElementById('plant13');
+const plant14 = document.getElementById('plant14');
+
 // Function to Drag Element (Complete)
 
 function dragElement(terrariumElement) {
@@ -7,6 +22,7 @@ function dragElement(terrariumElement) {
       pos4 = 0;
 
     terrariumElement.onpointerdown = pointerDrag;
+
     function pointerDrag(e) {
         e.preventDefault();
         console.log(e);
@@ -23,6 +39,7 @@ function dragElement(terrariumElement) {
         pos4 = e.clientY;
 
         console.log(pos1, pos2, pos3, pos4);
+
         terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
         terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
     }
@@ -35,4 +52,17 @@ function dragElement(terrariumElement) {
 
 //Event Listeners for Plant
 
-dragElement(document.getElementById('plant1'));
+dragElement(plant1);
+dragElement(plant2);
+dragElement(plant3);
+dragElement(plant4);
+dragElement(plant5);
+dragElement(plant6);
+dragElement(plant7);
+dragElement(plant8);
+dragElement(plant9);
+dragElement(plant10);
+dragElement(plant11);
+dragElement(plant12);
+dragElement(plant13);
+dragElement(plant14);
